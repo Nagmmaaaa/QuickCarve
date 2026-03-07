5545fe7992fb7d6f64b23c607f3263c288ef00d8
Nagmmaaa <nagmmaaakhan00@gmail.com>
2026-03-07 22:51:09 +0530

    auto create admin user

--- backend/qc_backend/settings.py
+++ backend/qc_backend/settings.py
@@ -114,3 +114,11 @@ SIMPLE_JWT = {
 }
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+import os
+from django.contrib.auth import get_user_model
+
+if os.environ.get("CREATE_SUPERUSER", "True") == "True":
+    User = get_user_model()
+    if not User.objects.filter(username="admin").exists():
+        User.objects.create_superuser("admin", "admin@example.com", "admin123")
