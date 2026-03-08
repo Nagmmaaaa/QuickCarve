842161716f9128c292b5b367fda36a88cff1b14e
Nagmmaaa <nagmmaaakhan00@gmail.com>
2026-03-08 22:52:42 +0530

    fix SECRET_KEY and whitenoise for local and render

--- backend/qc_backend/settings.py
+++ backend/qc_backend/settings.py
@@ -4,7 +4,10 @@ import os
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = os.environ.get(
+    "SECRET_KEY",
+    "django-insecure-local-dev-secret-key"
+)
 DEBUG = os.environ.get("DEBUG", "False") == "True"
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "*").split(",")
 
