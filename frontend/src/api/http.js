b9f1fc88f8f264b1d0dade0ebf943aa19e47135c
Nagmmaaa <nagmmaaakhan00@gmail.com>
2026-03-09 20:06:50 +0530

    connect frontend to render backend

--- frontend/src/api/http.js
+++ frontend/src/api/http.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://127.0.0.1:8000/api";
+export const BASE_URL = "https://quickcarve.onrender.com/api";
 
 const api = axios.create({ baseURL: BASE_URL });
 
