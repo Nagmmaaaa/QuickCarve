a72f48718c83a8814d2b05533f6b0cc3ce641912
Nagmmaaa <nagmmaaakhan00@gmail.com>
2026-03-09 14:16:59 +0530

    restore database

--- QuickCrave_Project/backend/cartapp/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Cart
-
-@admin.register(Cart)
-class CartAdmin(admin.ModelAdmin):
-    list_display = ('id', 'user', 'restaurant', 'item_name', 'item_price', 'quantity', 'created_at')
-    search_fields = ('item_name', 'user__username', 'restaurant__name')
-    list_filter = ('restaurant', 'created_at')
